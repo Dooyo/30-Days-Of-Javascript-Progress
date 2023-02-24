6da5227f8d04fa6a8f2fbdef3262cd58248bc541
Doyoo <dolphinmulugeta88@gmail.com>
2023-02-24 02:40:29 +0000

    added some of day 1

---
+++ Day-01/somemath.js
@@ -0,0 +1,13 @@
+let addition_demo = 2 + 3
+let subtraction_demo = 3- 2
+let multiplication_demo = 3*2
+let division_demo = 3 / 2
+let modulo_demo = 3 % 2
+let exponent_demo = 3**2
+
+console.log(addition_demo)
+console.log(subtraction_demo)
+console.log(multiplication_demo)
+console.log(division_demo)
+console.log(modulo_demo)
+console.log(exponent_demo)
